164acaae6258f71069e853d0a5578c73666b2769
Gabriele Canesi <g.canesi1@campus.unimib.it>
2021-12-28 23:04:00 +0100

    Aggiunti test

--- main.cpp
+++ main.cpp
@@ -2,28 +2,44 @@
 #include <vector>
 #include "SparseMatrix.h"
 
+
+struct pari{
+    bool operator()(int n){
+        return n % 2 == 0;
+    }
+};
+
+// Classe di prova come tipo di SparseMatrix
+class test_class{
+    int value;
+public:
+    test_class(int v){
+        value = v;
+    }
+};
+
 int main(int argc, char* argv[]) {
-
-    std::cout << "Ciao" << std::endl;
-
-    SparseMatrix<std::vector<int> > m;
-    SparseMatrix<std::vector<int> > mm = m;
 
     SparseMatrix<int> matrice(20, 20, -1);
 
-    matrice.set(1, 1, 34);
-    std::cout << matrice(2, 2) << std::endl;
-    std::cout << matrice(1, 1) << std::endl;
+    matrice.set(1, 1, 33);
+    matrice.set(15, 20, 24);
+    // Test sostuzione
+    matrice.set(1, 1, 10);
 
     SparseMatrix<int>::const_iterator it = matrice.begin();
     SparseMatrix<int>::const_iterator end = matrice.end();
 
     while(it != matrice.end()){
-        std::cout << it->data << std::endl;
-        std::cout << (*it).data << std::endl;
-        std::cout << it->m_i << ", " <<  it->m_j << std::endl;
+        std::cout << "("  << it->m_i << ", " << it->m_j << ") -> " << it->data << std::endl;
         ++it;
     }
+
+    std::cout << "Evaluate: " << evaluate(matrice, pari()) << std::endl;
+
+    SparseMatrix<test_class> test_c(10, 10, test_class(-2));
+
+
     std::cout << "Bella" << std::endl;
     return 0;
 }
